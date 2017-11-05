092ea398595a309842de18b45d0ec6fb9035bd70
Arun Jose <arunjose@qburst.com>
2017-11-06 01:37:35 +0530

    Image load and pull to refresh completed

--- Ainterest/Ainterest/Controllers/HomeController/HomeViewController.swift
+++ Ainterest/Ainterest/Controllers/HomeController/HomeViewController.swift
@@ -10,115 +10,86 @@ import UIKit
 import FileDownloadHelper
 
 class HomeViewController: UIViewController {
-    var itemsHieght = [100, 200, 520, 80, 105, 164, 78, 200, 312, 74]
-    var itemsWidth = [100, 400, 350, 40, 125, 100, 100, 150, 400, 50]
+    
     var postCollectionDataSource:PostCollectionDataSource? = nil
     var postCollectionDelegate:PostCollectionDelegate? = nil
-    var url:URL?
     static var postsList:[PostDetails] = []
+    var refreshControl:UIRefreshControl?
     
     @IBOutlet weak var postCollectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        test()
+        
+        if !AppDelegate.isTesting(){
+            intizialisePostCollectionView()
+            setupPullToRefresh()
+            getPostDetails()
+        }
+        
+    }
+    
+    func intizialisePostCollectionView(){
         if let layout = postCollectionView.collectionViewLayout as? PostsLayout {
             layout.delegate = self
         }
-        
         postCollectionDataSource = PostCollectionDataSource()
         postCollectionDelegate = PostCollectionDelegate()
-        
         postCollectionView.dataSource = postCollectionDataSource
         postCollectionView.delegate = postCollectionDelegate
         
-//        postCollectionView.reloadData()
-        // Do any additional setup after loading the view.
-        if !AppDelegate.isTesting(){
+    }
+    
+    func getPostDetails(){
+        
         APIController().getPostsDetails(completion: {postsList, error in
-            
             HomeViewController.postsList = postsList!
             DispatchQueue.main.async {
+                if self.refreshControl != nil{
+                    self.refreshControl?.endRefreshing()
+                }
                 self.postCollectionView.reloadData()
             }
         })
-        }
-    
+        
     }
-
+    
+    func setupPullToRefresh() {
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(refreshPostsCollection), for: .valueChanged)
+        if #available(iOS 10.0, *) {
+            postCollectionView.refreshControl = refreshControl
+        } else {
+            postCollectionView.addSubview(refreshControl!)
+        }
+    }
+    
+    func refreshPostsCollection(){
+        getPostDetails()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
     
-    @IBAction func testDownload(_ sender: Any) {
-        FileDownloadHelper.setCacheLimit(limit: 20) // Set Cache limit for download helper
-
-        let progress = {(progress:Float) -> (Void) in
-           // NSLog("progress:- \(progress)");
-        }
-        let completion = {(success:DefaultDownloadResponse) -> (Void) in
-            if success.error == nil{
-            NSLog("success:- \(success)");
-            let image = UIImage(data:success.data!,scale:1.0)
-            print("done")
-            }else{
-                NSLog("Failure:- \(success)");            }
-        }
-        let url = URL.init(string: "https://images.unsplash.com/photo-1464550883968-cec281c19761?ixlib=rb-0.3.5\u{0026}q=80\u{0026}fm=jpg\u{0026}crop=entropy\u{0026}s=4b142941bfd18159e2e4d166abcd0705")
-        
-        //        let url = URL.init(string: "http://www.otc.umd.edu/sites/default/files/documents/sample-license.pdf")
-        let downloadTask = FileDownloadHelper(withUrl: self.url!, Progress: progress, Completion: completion)
-        downloadTask.startDownload()
-        
-//        let downloadTask1 = FileDownloadHelper(withUrl: url!, Progress: progress, Completion: completion)
-//        downloadTask1.startDownload()
-//
-//        downloadTask.cancelDownload()
-        
-        
-    }
-//    func test(){
-//        let progress = {(progress:Float) -> (Void) in
-//            NSLog("progress:- \(progress)");
-//        }
-//        let completion = {(success:DefaultDownloadResponse) -> (Void) in
-//            NSLog("success:- \(success)");
-//            let image = UIImage(data:success.data!,scale:1.0)
-//            NSLog("success:- \(success)");
-//        }
-//        let url = URL.init(string: "https://images.unsplash.com/photo-1464550883968-cec281c19761?ixlib=rb-0.3.5\u{0026}q=80\u{0026}fm=jpg\u{0026}crop=entropy\u{0026}s=4b142941bfd18159e2e4d166abcd0705")
-//
-////        let url = URL.init(string: "http://www.otc.umd.edu/sites/default/files/documents/sample-license.pdf")
-//
-//        FileDownloadHelper.sharedInstance.startDownload(FromURL: url!, withProgress:progress, withCompleteion: completion )
-    
-//    }
-    func testCancel(){
-        let url = URL.init(string: "https://images.unsplash.com/photo-1464550883968-cec281c19761?ixlib=rb-0.3.5\u{0026}q=80\u{0026}fm=jpg\u{0026}crop=entropy\u{0026}s=4b142941bfd18159e2e4d166abcd0705")
-        
-        //        let url = URL.init(string: "http://www.otc.umd.edu/sites/default/files/documents/sample-license.pdf")
-        
-//        FileDownloadHelper.sharedInstance.cancelDownload(FromURL:url)
-    }
-
 }
 
 
 extension HomeViewController : PostsLayoutDelegate {
     
-    // 1. Returns the photo height
+    // 1. Returns the posts height
     func collectionView(_ collectionView: UICollectionView, sizeForPhotoAtIndexPath indexPath:IndexPath) -> CGSize {
         let postDetail = HomeViewController.postsList[indexPath.item]
         return  CGSize(width: postDetail.imageWidth!, height: postDetail.imageHeight!)
--- Ainterest/Ainterest/Controllers/HomeController/PostCollectionDataSource.swift
+++ Ainterest/Ainterest/Controllers/HomeController/PostCollectionDataSource.swift
@@ -11,38 +11,43 @@ import UIKit
 import FileDownloadHelper
 
 public class PostCollectionDataSource: NSObject, UICollectionViewDataSource {
-
+    
     let reuseIdentifier = "PostCollectionCell" // also enter this string as the cell identifier in the storyboard
-    var items = [100, 200, 50, 80, 105, 164, 78, 200, 312, 74]
-
     
     
     // MARK: - UICollectionViewDataSource protocol
     // tell the collection view how many cells to make
     public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
         return HomeViewController.postsList.count
     }
     
     // make a cell for each cell index path
     public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        // get a reference to our storyboard cell
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! PostCollectionViewCell
         
-        // Use the outlet in our custom class to get a reference to the UILabel in the cell
         let imageUrl = HomeViewController.postsList[indexPath.item].imageUrl!
         let downloadTask = FileDownloadHelper(withUrl:imageUrl , Progress: {progress in
         }, Completion: {response in
             if response.error == nil{
                 DispatchQueue.main.async {
-                    cell.postImageView.image = UIImage(data:response.data!,scale:1.0)
-                }
+                    if let image = UIImage(data:response.data!,scale:1.0)     {
+                        if let cellNew: PostCollectionViewCell = collectionView.cellForItem(at: indexPath) as? PostCollectionViewCell {
+                            cellNew.postImageView.image = image
+                        }else{
+                            cell.postImageView.image = image
+                        }
+                    }else{
+                        print("Image load failed due to invalid data")
+                    }
+                };
+            }else{
+                print("Download Failed: ",response.error!)
             }
-            
         })
+        cell.postImageView.image = nil
         downloadTask.startDownload()
-//        cell.postImageView.image = UIImage(named: "post")
-        cell.backgroundColor = UIColor.cyan // make cell more visible in our example project
         
         return cell
     }
